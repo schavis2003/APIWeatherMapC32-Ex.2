8f0774a33e0209eed321963ec7ce55fecc7f6106
schavis2022 <schavis2022>
2023-01-13 12:51:51 -0500

    APIWeatherC32 Ex.2 Error

--- Program.cs
+++ Program.cs
@@ -1,9 +1,25 @@
-﻿namespace APIWeatherMapC32_Ex._2;
+﻿
+using System;
+using System.IO;
+using Newtonsoft.Json.Linq;  
 
-internal class Program
+namespace APIWeatherMapC32_Ex._2;
+
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
+        
+        string key = File.ReadAllText("appsettings.json");
+        string APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
+
+        Console.WriteLine("Please enter you zipcode");
+
+        var zipCode = Console.ReadLine();
+
+        var apiCall = $"https://api.openweathermap.org/data/2.5/weather?{zipCode}&units=imperial&appid={APIKey}";
         Console.WriteLine();
+
+        Console.WriteLine($"It is currently  {WeatherMap.GetTemp(apiCall)} degrees F in your location.");
     }
 }
---
+++ WeatherMap.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIWeatherMapC32_Ex._2
+{
+    public class WeatherMap
+    {
+        public static double GetTemp(string apiCall)
+        {
+            var client = new HttpClient();
+
+            var response = client.GetStringAsync(apiCall).Result;
+
+            var temp = double.Parse(JObject.Parse(response)["main"]["temp"].ToString());
+
+            return temp; 
+        }
+    }
+}
